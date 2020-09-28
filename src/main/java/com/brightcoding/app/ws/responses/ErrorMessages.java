33c8ca87060df82abb95c1dc292ea4ede3953ab9
Laabidi-Wafa <laabidiwafa199@gmail.com>
2020-09-28 22:21:45 +0100

    manipulation d'ensemble d'excepptions

--- src/main/java/com/brightcoding/app/ws/responses/ErrorMessages.java
+++ src/main/java/com/brightcoding/app/ws/responses/ErrorMessages.java
@@ -1,7 +1,7 @@
 package com.brightcoding.app.ws.responses;
 //class dédié pour les exceptions
 public enum ErrorMessages {
-	MISSING_REQUIRED_FIELD("Missing  required  field."), //le champ n'existe pas
+	MISSING_REQUIRED_FIELD("Missing required field."), //le champ n'existe pas
 	RECORD_ALREADY_EXISTS("record already exists."), //le champ deja existe
 	INTERNAL_SERVER_ERROR("internal server error."),  
 	NO_RECORD_FOUND("no record found."); //objet n'existe pas
